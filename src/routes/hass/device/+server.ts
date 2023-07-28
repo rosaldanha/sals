294ddca849dcaab755baf7115598a0a4c68f6a5a
Ricardo Saldanha <ricardosaldanha@mpf.mp.br>
2023-07-28 13:10:35 -0300

    change to use sveltekit fetch function

--- src/routes/hass/device/+server.ts
+++ src/routes/hass/device/+server.ts
@@ -6,8 +6,8 @@ import {buildDeviceObj} from '$lib/server/hassfunctions';
 import type { Device, Entity } from '$lib/hassinterfaces';
 
 
-export async function POST({ request }) {
+export async function POST({ request, fetch }) {
     const { entityId } = await request.json();
-    const result = await buildDeviceObj(entityId);
+    const result = await buildDeviceObj(entityId, fetch);
     return json(result);
 }
