5508daec37cc12b6dc9ef2861ce266bafa8ea994
Ricardo Saldanha <ricardosaldanha@mpf.mp.br>
2023-07-28 12:59:09 -0300

    change to use the sveltekit fetch

--- src/routes/paineis/[panelName]/+page.server.ts
+++ src/routes/paineis/[panelName]/+page.server.ts
@@ -4,10 +4,10 @@ import type {  PanelInterface } from '$lib/panel.js';
 //TODO: change from $env/dynamic to $env/private
 import { env } from '$env/dynamic/public';
 /** @type {import('./$types').PageServerLoad} */
-export async function load({ params }) {    
-    const panelInterface: PanelInterface = await getPanelByName(params.panelName);
-    const entitiesIdToChoose = await getAllEntities();
-    const esphomeEntitiesToWatch = await getEspHomeEntitiesIdToWatch();
+export async function load({ params, fetch }) {    
+    const panelInterface: PanelInterface = await getPanelByName(params.panelName, fetch);
+    const entitiesIdToChoose = await getAllEntities(fetch);
+    const esphomeEntitiesToWatch = await getEspHomeEntitiesIdToWatch(fetch);
     
     
     return {panelInterface,
